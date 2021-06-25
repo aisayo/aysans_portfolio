1ca4ae906c9c3b8ba04b129aa91746bd4a690c24
Aysan Isayo <aisayo@icloud.com>
2021-06-25 16:06:43 -0700

    refactored menu bar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import NavLink from "./navLink";
+import MenuBar from "./menuBar";
 import { Link } from "gatsby";
 
 const NavBar = () => {
@@ -15,38 +16,25 @@ const NavBar = () => {
           </Link>
         </div>
         <div className="px-4 cursor-pointer md:hidden">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M4 6h16M4 12h16M4 18h16"
-            />
-          </svg>
+          <MenuBar />
         </div>
         <div id="navbar-links">
           <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
-          <NavLink 
-            url="/about"
-            style="p-1 fab text-lg leading-lg text-black opacity-75"
-            text="About"
-          />
-          <NavLink 
-            url="/projects"
-            style="p-1 fab text-lg leading-lg text-black opacity-75"
-            text="Projects"
-          />
-          <NavLink 
-            url="/contact"
-            style="p-1 fab text-lg leading-lg text-black opacity-75"
-            text="Contact"
-          />
+            <NavLink
+              url="/about"
+              style="p-1 fab text-lg leading-lg text-black opacity-75"
+              text="About"
+            />
+            <NavLink
+              url="/projects"
+              style="p-1 fab text-lg leading-lg text-black opacity-75"
+              text="Projects"
+            />
+            <NavLink
+              url="/contact"
+              style="p-1 fab text-lg leading-lg text-black opacity-75"
+              text="Contact"
+            />
           </ul>
         </div>
       </div>
